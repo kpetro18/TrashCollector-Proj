6a2aa421fd6a382d224ab0d498f18124bfaf2c72
kevin petroviak <kpetro18@gmail.com>
2021-02-02 14:30:53 -0600

    added member variables for Employee/Customer Models

---
+++ TrashCollector/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        public int PaymentDue { get; set; }
+        public string WeeklyPickupDay { get; set; }
+        public bool AdditionalPickup { get; set; }
+        public string ExtraPickupDay { get; set; }
+        public bool SuspendedPickup { get; set; }
+        public string SuspendedDays { get; set; }
+    }
+}
---
+++ TrashCollector/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Models
+{
+    public class Employee
+    {
+        public List<string> DailyPickupLocations { get; set; }//add variable List<> of pickup locations for the day -- using google maps API?
+        public int ZipCode { get; set; } //might need additional variables for google map API
+        public string CurrentDayOfWeek { get; set; }
+        public bool CompletedPickup { get; set; } //if completed pickup add charge to customer
+
+    }
+}
